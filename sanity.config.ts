b40a620b74990bbda432f9e72fb95518c1d56de9
Joe Glanville <josephglanville@gmail.com>
2023-08-17 15:32:25 +0100

    Inital Commit

---
+++ sanity.config.ts
@@ -0,0 +1,19 @@
+import {defineConfig} from 'sanity'
+import {deskTool} from 'sanity/desk'
+import {schemaTypes} from './schemas'
+
+export default defineConfig({
+  name: 'default',
+  title: 'Sanity Next.js Site',
+
+  projectId: 'yno2oiby',
+  dataset: 'production',
+
+  basePath: "/studio",
+
+  plugins: [deskTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
